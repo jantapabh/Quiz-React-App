c6d19da11221568deda2ea6dbafed0fd82fbf6e3
devInternPir <devintern@pirsquare.net>
2021-03-04 10:41:21 +0700

    add commit first

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,77 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
-function App() {
+//Componrnts
+import QuestionCard from "./components/QueastionCard";
+
+//API
+import { fetchQuizQuestions } from "./API";
+
+//Types
+import { Difficulty, QuestionState } from "./API";
+
+type AnswerObject = {
+  question: string;
+  answer: string;
+  correct: boolean;
+  correctAnswer: string;
+};
+
+const TOTAL_QUESTION = 10;
+
+const App = () => {
+  const [loading, setLoading] = useState<boolean>(false);
+  const [questions, setQuestions] = useState<QuestionState[]>([]);
+  const [number, setNumber] = useState<number>(0);
+  const [userAnswers, setUserAnswers] = useState<AnswerObject[]>([]);
+  const [score, setScore] = useState<number>(0);
+  const [gameOver, setGameOver] = useState<boolean>(true);
+
+  console.log(fetchQuizQuestions(TOTAL_QUESTION, Difficulty.EASY));
+  console.log("Question ---> ", questions);
+
+  const startIrivia = async () => {
+    setLoading(true);
+    setGameOver(false);
+
+    const newQuestions = await fetchQuizQuestions(
+      TOTAL_QUESTION,
+      Difficulty.EASY
+    );
+
+    setQuestions(newQuestions);
+    setScore(0);
+    setUserAnswers([]);
+    setNumber(0);
+    setLoading(false);
+  };
+
+  const checkAnswer = (e: React.MouseEvent<HTMLButtonElement>) => {};
+
+  const nextQuestion = () => {};
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>React Quiz</h1>
+      {gameOver || userAnswer.length === TOTAL_QUESTION ? (
+        <button className="start" onClick={startIrivia}>
+          Start Quiz
+        </button>
+      ) : null}
+      {!gameOver ? <p className="score">Score: </p> : null}
+      {loading && <p>Loading Question ...</p>}
+      <QuestionCard
+        questionNr={number + 1}
+        totalQuestions={TOTAL_QUESTION}
+        question={questions[number].question}
+        answers={questions[number].answers}
+        userAnswer={userAnswers ? userAnswers[number] : undefined}
+        callback={checkAnswer}
+      />
+      <button className="next">Next .. Question</button>
     </div>
   );
-}
+};
 
 export default App;
